3ff67474c544eca232a5de79fc9a0b2af9773206
unknown <tim.mota21@gmail.com>
2024-04-17 18:58:29 +1000

    coursel

--- src/Row.js
+++ src/Row.js
@@ -4,6 +4,7 @@ import axios from "./axios";
 
 const Row = ({ title, fetchURL, isLargeRow = false, isBigger = false }) => {
   const [movies, setMovies] = useState([]);
+  const [scrollX, setScrollX] = useState(0); // Track horizontal scroll position
 
   const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -17,32 +18,54 @@ const Row = ({ title, fetchURL, isLargeRow = false, isBigger = false }) => {
     fetchData();
   }, [fetchURL]);
 
-  console.log(movies);
+  const handleLeftArrowClick = (id) => {
+    const row = document.getElementById(`row-container-${id}`);
+    row.scrollLeft -= row.offsetWidth / 2; // Scroll half the width of the container
+    setScrollX(row.scrollLeft);
+  };
+  
+  const handleRightArrowClick = (id) => {
+    const row = document.getElementById(`row-container-${id}`);
+    row.scrollLeft += row.offsetWidth / 2; // Scroll half the width of the container
+    setScrollX(row.scrollLeft);
+  };
 
   return (
     <div className="row">
       <h2>{title}</h2>
 
-      <div className="row__posters">
+      <div className="row__posters" id={`row-container-${title}`}>
         {movies &&
           movies.map((movie) =>
             (isLargeRow && movie.poster_path) ||
             (isBigger && movie.backdrop_path) ? (
-              <>
-                <img
-                  className={`row__poster ${isLargeRow && "row__posterLarge"} ${
-                    isBigger && "isBiggerPoster"
-                  }`}
-                  key={movie.id}
-                  src={`${base_url}${
-                    isLargeRow ? movie.poster_path : movie.backdrop_path
-                  }`}
-                  alt={movie.name}
-                />
-              </>
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"} ${
+                  isBigger && "isBiggerPoster"
+                }`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
             ) : null
           )}
       </div>
+
+      {/* Navigation Buttons */}
+      <button
+        className="arrow left"
+        onClick={() => handleLeftArrowClick(title)}
+      >
+        {"<"}
+      </button>
+      <button
+        className="arrow right"
+        onClick={() => handleRightArrowClick(title)}
+      >
+        {">"}
+      </button>
     </div>
   );
 };
