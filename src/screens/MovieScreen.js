e431f34d71edda91770476b66380db8502c3651e
unknown <tim.mota21@gmail.com>
2024-05-19 18:29:00 +1000

    movie display progres pt 2

--- src/screens/MovieScreen.js
+++ src/screens/MovieScreen.js
@@ -3,9 +3,16 @@ import { Link, useParams } from "react-router-dom";
 import axios from "../axios.js";
 import Avatar from "../../src/assets/download.png";
 import "./MovieScreen.css";
+import LoadingScreen from "./LoadingScreen.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import Movie from "../Movie.js";
 
 const MovieScreen = () => {
   const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
 
   useEffect(() => {
     const fetchMovie = async () => {
@@ -13,11 +20,12 @@ const MovieScreen = () => {
         const response = await axios.get(
           `https://api.themoviedb.org/3/movie/${id}?api_key=3ef16179b4be2afc7c81bf6333abb5b5&language=en-US`
         );
-        const data = response.data;
-        console.log(data);
-        // set your state here with the data, if you want to display it
+        setMovie(response.data);
+        setLoading(false);
+        getRecommendedMovies();
       } catch (error) {
         console.error("Error fetching movie:", error);
+        setLoading(false);
       }
     };
 
@@ -25,6 +33,40 @@ const MovieScreen = () => {
       fetchMovie();
     }
   }, [id]);
+
+  async function getRecommendedMovies() {
+    const res = await axios.get(
+      movie
+        ? `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=3ef16179b4be2afc7c81bf6333abb5b5&language=en-US&page=1`
+        : `https://api.themoviedb.org/3/tv/${id}/recommendations?api_key=3ef16179b4be2afc7c81bf6333abb5b5&language=en-US&page=1`
+    );
+
+    let data;
+    let res2;
+
+    if (res.data.results.length === 0) {
+      console.log("using similar instead");
+      res2 = await axios.get(
+        movie
+          ? `https://api.themoviedb.org/3/movie/${id}/similar?api_key=3ef16179b4be2afc7c81bf6333abb5b5&language=en-US&page=1`
+          : `https://api.themoviedb.org/3/tv/${id}/similar?api_key=3ef16179b4be2afc7c81bf6333abb5b5&language=en-US&page=1`
+      );
+      data = res2.data.results.slice(0, 4);
+      return setRecommendedMovies(data);
+    }
+
+    data = res.data.results.slice(0, 4);
+
+    setRecommendedMovies(data);
+  }
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
+  if (!movie) {
+    return <div>Movie not found.</div>;
+  }
 
   return (
     <>
@@ -52,9 +94,60 @@ const MovieScreen = () => {
           </div>
         </div>
       </nav>
-     
 
-     
+      <div className="movie-screen" style={{
+            backgroundImage: loading
+              ? null
+              : movie?.backdrop_path &&
+                `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
+          }}>
+        <div className="container_movie">
+          <div className="movie-image">
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+            />
+          </div>
+          <div className="movie-details">
+            <h1 className="movie-title">{movie.title}</h1>
+            <div className="movie-info">
+              <p className="movie-language">
+                {" "}
+                {movie.original_language?.toUpperCase()}{" "}
+              </p>
+              <p className="movie-genres">
+                {movie.genres.map((genre) => genre.name).join(", ")}
+              </p>
+              <p className="movie-runtime">
+                {" "}
+                <FontAwesomeIcon icon={faClock} /> {movie.runtime}
+              </p>
+            </div>
+            <div className="movie-overview">
+              <h2>Overview</h2>
+              <p>{movie.overview}</p>
+            </div>
+            <div className="movie-meta">
+              <div className="movie-release-date">
+                Release Date: {movie.release_date}
+              </div>
+              <div className="movie-rating">Rating: {movie.vote_average}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="recco-container">
+        <h2 className="recco-title">Recommended Movies</h2>
+        <div className="recommended-movies">
+          {recommendedMovies.map((recommendedMovie) => (
+            <Movie
+              key={recommendedMovie.id}
+              title={recommendedMovie.title}
+              posterPath={recommendedMovie.poster_path}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
